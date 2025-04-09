0219ea758445dbbf9d94a3466e4a6f4ca949fad8
sekisamu <22696121+sekisamu@users.noreply.github.com>
2025-04-09 18:02:55 +0800

    add UniswapV2FactorySendRaw

---
+++ test/UniswapV2FactorySendRaw.js
@@ -0,0 +1,142 @@
+const {chai, expect } = require("chai");
+const { expandTo18Decimals } = require('./shared/utilities');
+const hre = require("hardhat");
+const { 
+  BigInt,
+  getBigInt,
+  getAddress,
+  keccak256,
+  AbiCoder,
+  toUtf8Bytes,
+  getCreate2Address
+} = require('ethers')
+
+
+const TEST_ADDRESSES = [
+  '0x1000000000000000000000000000000000000000',
+  '0x2000000000000000000000000000000000000000'
+];
+
+const TOTAL_SUPPLY = expandTo18Decimals(10000)
+
+describe('UniswapV2Factory', function () {
+
+let token;
+let wallet;
+let other;
+let factory;
+ 
+  beforeEach(async function () {
+
+    // NOTE: It's not necessary to deploy the pair contract
+    // while pallet-revive now require the code exists on chain
+    // before it is deployed inside a contract.
+
+    // require("dotenv").config();
+    // const localkey = process.env.LOCAL_PRIV_KEY;
+    // const ahkey = process.env.AH_PRIV_KEY;
+    const provider = new ethers.JsonRpcProvider(hre.network.config.url);
+    const walletKey = hre.network.config.accounts[0];
+    wallet = new ethers.Wallet(walletKey, provider);
+
+    // Deploying the pair contract using the factory
+    const UniswapV2Pair = require("../artifacts-pvm/contracts/UniswapV2Pair.sol/UniswapV2Pair.json");
+    const pairFactory = new ethers.ContractFactory(UniswapV2Pair.abi, UniswapV2Pair.bytecode, wallet);
+    pair = await pairFactory.deploy();
+    await pair.waitForDeployment();
+
+    // Deploying the pair with raw transaction
+    const UniswapV2Pair1 = await ethers.getContractFactory("UniswapV2Pair");
+    let bytecode = UniswapV2Pair1.bytecode;
+    const rawTx = {
+      to: null,
+      from: wallet.address,
+      data: bytecode,
+      value: 0, 
+    }
+  
+    const tx = await wallet.sendTransaction(rawTx);
+    const receipt = await tx.wait();
+  
+    const pairAddress = receipt.contractAddress;
+    console.log("pairAddress", pairAddress);
+
+    const ERC20 = require("../artifacts-pvm/contracts/test/ERC20.sol/ERC20.json");
+    const erc20Factory = new ethers.ContractFactory(ERC20.abi, ERC20.bytecode, wallet);
+    token = await erc20Factory.deploy(TOTAL_SUPPLY);
+    await token.waitForDeployment();
+
+    const UniswapV2Factory = require("../artifacts-pvm/contracts/UniswapV2Factory.sol/UniswapV2Factory.json");
+    const factoryFactory = new ethers.ContractFactory(UniswapV2Factory.abi, UniswapV2Factory.bytecode, wallet);
+    factory = await factoryFactory.deploy(wallet.address);
+    await factory.waitForDeployment();
+
+  });
+
+
+  it('feeTo, feeToSetter, allPairsLength', async function() {
+    expect(await factory.feeTo()).to.eq(ethers.ZeroAddress);
+    expect(await factory.feeToSetter()).to.eq(wallet.address);
+    expect(await factory.allPairsLength()).to.eq(0);
+  });
+
+  // async function createPair(tokens) {
+  //   const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
+  //   let pair = await UniswapV2Pair.deploy({
+  //     allowUnlimitedInitCodeSize: true,
+  //   });
+  //   await pair.waitForDeployment();
+
+  //   // const bytecode = `0x${UniswapV2Pair.evm.bytecode.object}`;
+  //   const bytecode = UniswapV2Pair.bytecode;
+  //   const create2Address = getCreate2Address(factory.address, tokens, bytecode);
+  //   console.log("Predicted create2Address of Pair", create2Address);
+
+  //   await expect(factory.createPair(tokens[0], tokens[1]))
+  //     .to.emit(factory, 'PairCreated')
+  //     .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, 1n);
+  //   console.log("factory address", await factory.getAddress());
+  //   await factory.createPair(tokens[0], tokens[1]);
+  //   await expect(factory.createPair(...tokens)).to.be.reverted; // UniswapV2: PAIR_EXISTS
+  //   await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted; // UniswapV2: PAIR_EXISTS
+  //   expect(await factory.getPair(...tokens)).to.eq(create2Address);
+  //   expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address);
+  //   expect(await factory.allPairs(0)).to.eq(create2Address);
+  //   expect(await factory.allPairsLength()).to.eq(1);
+
+  //   // const pair = new Contract(create2Address, JSON.stringify(UniswapV2Pair.abi), provider);
+  //   // expect(await pair.factory()).to.eq(factory.address);
+  //   // expect(await pair.token0()).to.eq(TEST_ADDRESSES[0]);
+  //   // expect(await pair.token1()).to.eq(TEST_ADDRESSES[1]);
+  // }
+
+  // it('createPair', async function() {
+  //   await createPair(TEST_ADDRESSES);
+  // });
+
+  // // it('createPair:reverse', async function() {
+  // //   await createPair(TEST_ADDRESSES.slice().reverse());
+  // // });
+
+  // // it('createPair:gas', async function() {
+  // //   const tx = await factory.createPair(...TEST_ADDRESSES);
+  // //   const receipt = await tx.wait();
+  // //   expect(receipt.gasUsed).to.eq(2512920);
+  // // });
+
+  // it('setFeeTo', async function() {
+  //   await expect(factory.connect(other).setFeeTo(other.address))
+  //     .to.be.revertedWith('UniswapV2: FORBIDDEN');
+  //   await factory.setFeeTo(wallet.address);
+  //   expect(await factory.feeTo()).to.eq(wallet.address);
+  // });
+
+  // it('setFeeToSetter', async function() {
+  //   await expect(factory.connect(other).setFeeToSetter(other.address))
+  //     .to.be.revertedWith('UniswapV2: FORBIDDEN');
+  //   await factory.setFeeToSetter(other.address);
+  //   expect(await factory.feeToSetter()).to.eq(other.address);
+  //   await expect(factory.setFeeToSetter(wallet.address))
+  //     .to.be.revertedWith('UniswapV2: FORBIDDEN');
+  // });
+});
