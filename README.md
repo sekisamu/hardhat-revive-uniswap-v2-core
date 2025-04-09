db07b1d3ad49b0065e24b8b9eb5abd7fcb70f2a9
sekiseki <22696121+sekisamu@users.noreply.github.com>
2025-04-09 23:26:22 +0800

    bypass code size check (#1)
    * add UniswapV2FactorySendRaw
    
    * update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # hardhat-revive-uniswap-v2-core
+
 ## Prerequisites
+
 Ensure that you have substrate-node, eth-rpc and local resolc binaries on your local machine. If not, follow these instructions to install them:
 
 ```bash
@@ -8,6 +10,7 @@ cd polkadot-sdk
 cargo build --bin substrate-node --release
 cargo build -p pallet-revive-eth-rpc --release
 ```
+
 Once the build is complete, you will find both binaries in the `./target/release` directory.
 
 For resolc's installation, please refer to the [resolc's README](https://github.com/paritytech/revive/blob/main/README.md).
@@ -19,18 +22,25 @@ Start the network by running:
 ```
 
 ## How to Initialize
+
 ```bash
-git clone https://github.com/sekisamu/hardhat-revive-storage
-npm install
+git clone https://github.com/sekisamu/hardhat-revive-uniswap-v2-core
+cd hardhat-revive-uniswap-v2-core
+git checkout code-size-limit
+pnpm install
 ```
+
 Open the `hardhat.config.js` file and update the following fields under networks -> hardhat:
+
 ```
 nodeBinaryPath: Set this to the local path of your substrate-node binary.
 
 adapterBinaryPath: Set this to the local path of your eth-rpc binary.
 
 ```
+
 And add the following fields under resolc -> settings:
+
 ```
 compilerPath: Set this to the local path of your resolc binary.
 ```
@@ -38,11 +48,13 @@ compilerPath: Set this to the local path of your resolc binary.
 Ensure that both paths correctly point to the respective executable files.
 
 How to Test
+
 ```bash
 # For PolkaVM chains
-USE_RESOLC=true npx hardhat test --network polkavm
+npx hardhat test --network polkavm
 
 # For EVM chains
 npx hardhat test --network sepolia
 ```
+
 Note: You need to restart the network and rpc endpoint after each test (For PolkaVM chains).
--- test/UniswapV2Factory.js
+++ test/UniswapV2Factory.js
@@ -27,16 +27,20 @@ let other;
 let factory;
  
   beforeEach(async function () {
-    const ERC20 = await ethers.getContractFactory("ERC20");
-
-    token = await ERC20.deploy(TOTAL_SUPPLY);
-    await token.waitForDeployment();
-    [wallet, other] = await ethers.getSigners();
 
     // NOTE: It's not necessary to deploy the pair contract
     // while pallet-revive now require the code exists on chain
     // before it is deployed inside a contract.
+
+
+    [wallet, other] = await ethers.getSigners();
+
     const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
+    const ERC20 = await ethers.getContractFactory("ERC20");
+    token = await ERC20.deploy(TOTAL_SUPPLY);
+    await token.waitForDeployment();
+
+
     let pair = await UniswapV2Pair.deploy({
       allowUnlimitedInitCodeSize: true,
     });
@@ -45,6 +49,7 @@ let factory;
     const UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
     factory = await UniswapV2Factory.deploy(wallet.address);
     await factory.waitForDeployment();
+
   });
 
 
@@ -54,63 +59,63 @@ let factory;
     expect(await factory.allPairsLength()).to.eq(0);
   });
 
-  async function createPair(tokens) {
-    const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
-    let pair = await UniswapV2Pair.deploy({
-      allowUnlimitedInitCodeSize: true,
-    });
-    await pair.waitForDeployment();
+//   async function createPair(tokens) {
+//     const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
+//     let pair = await UniswapV2Pair.deploy({
+//       allowUnlimitedInitCodeSize: true,
+//     });
+//     await pair.waitForDeployment();
 
-    // const bytecode = `0x${UniswapV2Pair.evm.bytecode.object}`;
-    const bytecode = UniswapV2Pair.bytecode;
-    const create2Address = getCreate2Address(factory.address, tokens, bytecode);
-    console.log("Predicted create2Address of Pair", create2Address);
+//     // const bytecode = `0x${UniswapV2Pair.evm.bytecode.object}`;
+//     const bytecode = UniswapV2Pair.bytecode;
+//     const create2Address = getCreate2Address(factory.address, tokens, bytecode);
+//     console.log("Predicted create2Address of Pair", create2Address);
 
-    await expect(factory.createPair(tokens[0], tokens[1]))
-      .to.emit(factory, 'PairCreated')
-      .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, 1n);
-    console.log("factory address", await factory.getAddress());
-    await factory.createPair(tokens[0], tokens[1]);
-    await expect(factory.createPair(...tokens)).to.be.reverted; // UniswapV2: PAIR_EXISTS
-    await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted; // UniswapV2: PAIR_EXISTS
-    expect(await factory.getPair(...tokens)).to.eq(create2Address);
-    expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address);
-    expect(await factory.allPairs(0)).to.eq(create2Address);
-    expect(await factory.allPairsLength()).to.eq(1);
+//     await expect(factory.createPair(tokens[0], tokens[1]))
+//       .to.emit(factory, 'PairCreated')
+//       .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, 1n);
+//     console.log("factory address", await factory.getAddress());
+//     await factory.createPair(tokens[0], tokens[1]);
+//     await expect(factory.createPair(...tokens)).to.be.reverted; // UniswapV2: PAIR_EXISTS
+//     await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted; // UniswapV2: PAIR_EXISTS
+//     expect(await factory.getPair(...tokens)).to.eq(create2Address);
+//     expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address);
+//     expect(await factory.allPairs(0)).to.eq(create2Address);
+//     expect(await factory.allPairsLength()).to.eq(1);
 
-    // const pair = new Contract(create2Address, JSON.stringify(UniswapV2Pair.abi), provider);
-    // expect(await pair.factory()).to.eq(factory.address);
-    // expect(await pair.token0()).to.eq(TEST_ADDRESSES[0]);
-    // expect(await pair.token1()).to.eq(TEST_ADDRESSES[1]);
-  }
+//     // const pair = new Contract(create2Address, JSON.stringify(UniswapV2Pair.abi), provider);
+//     // expect(await pair.factory()).to.eq(factory.address);
+//     // expect(await pair.token0()).to.eq(TEST_ADDRESSES[0]);
+//     // expect(await pair.token1()).to.eq(TEST_ADDRESSES[1]);
+//   }
 
-  it('createPair', async function() {
-    await createPair(TEST_ADDRESSES);
-  });
+//   it('createPair', async function() {
+//     await createPair(TEST_ADDRESSES);
+//   });
 
-  // it('createPair:reverse', async function() {
-  //   await createPair(TEST_ADDRESSES.slice().reverse());
-  // });
+//   // it('createPair:reverse', async function() {
+//   //   await createPair(TEST_ADDRESSES.slice().reverse());
+//   // });
 
-  // it('createPair:gas', async function() {
-  //   const tx = await factory.createPair(...TEST_ADDRESSES);
-  //   const receipt = await tx.wait();
-  //   expect(receipt.gasUsed).to.eq(2512920);
-  // });
+//   // it('createPair:gas', async function() {
+//   //   const tx = await factory.createPair(...TEST_ADDRESSES);
+//   //   const receipt = await tx.wait();
+//   //   expect(receipt.gasUsed).to.eq(2512920);
+//   // });
 
-  it('setFeeTo', async function() {
-    await expect(factory.connect(other).setFeeTo(other.address))
-      .to.be.revertedWith('UniswapV2: FORBIDDEN');
-    await factory.setFeeTo(wallet.address);
-    expect(await factory.feeTo()).to.eq(wallet.address);
-  });
+//   it('setFeeTo', async function() {
+//     await expect(factory.connect(other).setFeeTo(other.address))
+//       .to.be.revertedWith('UniswapV2: FORBIDDEN');
+//     await factory.setFeeTo(wallet.address);
+//     expect(await factory.feeTo()).to.eq(wallet.address);
+//   });
 
-  it('setFeeToSetter', async function() {
-    await expect(factory.connect(other).setFeeToSetter(other.address))
-      .to.be.revertedWith('UniswapV2: FORBIDDEN');
-    await factory.setFeeToSetter(other.address);
-    expect(await factory.feeToSetter()).to.eq(other.address);
-    await expect(factory.setFeeToSetter(wallet.address))
-      .to.be.revertedWith('UniswapV2: FORBIDDEN');
-  });
+//   it('setFeeToSetter', async function() {
+//     await expect(factory.connect(other).setFeeToSetter(other.address))
+//       .to.be.revertedWith('UniswapV2: FORBIDDEN');
+//     await factory.setFeeToSetter(other.address);
+//     expect(await factory.feeToSetter()).to.eq(other.address);
+//     await expect(factory.setFeeToSetter(wallet.address))
+//       .to.be.revertedWith('UniswapV2: FORBIDDEN');
+//   });
 });
